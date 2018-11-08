9cacfe72319d6c010ccce5926c10657caf570834
Matteo Brancaleoni <mbrancaleoni@voismart.it>
2018-11-08 10:03:46 +0100

    bump min release needed

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  rename_deprecated_at: "1.5.0"
 ]
